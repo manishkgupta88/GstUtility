93a54160ab08364f0e48509361301b2f990d9a55
manishkgupta88 <manishkgupta88@gmail.com>
2025-03-07 21:59:02 +0530

    trigger file

--- src/main/java/org/manitech/Trigger.java
+++ src/main/java/org/manitech/Trigger.java
@@ -3,26 +3,20 @@ package org.manitech;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.manitech.forms.IntroForm;
-import org.manitech.service.ExcelConsolidator;
-import org.manitech.util.Constants;
-import org.manitech.util.Helper;
 
 import javax.swing.*;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.net.URISyntaxException;
 
 
 public class Trigger {
     private static final Logger logger = LogManager.getLogger(Trigger.class);
 
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) throws Exception {
         logger.info("Starting trigger");
         String folderPath = "Q:\\src\\intellij\\GstUtility\\src\\resources\\gst\\files\\";
         logger.info("Using path: " + folderPath);
         triggerForms();
-        //ExcelConsolidator consolidator = new ExcelConsolidator();
-        //consolidator.consolidate(folderPath);
+//        ExcelConsolidator consolidator = new ExcelConsolidator();
+//        consolidator.consolidate(folderPath);
         logger.info("Completed");
     }
 
